941dd6bffa27776e765d7a9df34e8fb3c61dceb0
Yasiru Randika <43146170+YasiruRandika@users.noreply.github.com>
2024-06-07 20:09:42 +0530

    Remove main.py file

---
+++ Dags/kafka_stream.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 6, 7, 0, 0, 0)
+}
+
+def streaming_data_from_api():
+    pass
+
+with DAG('user_automation',
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False
+) as dag:
+
+    streaming_task = PythonOperator(
+        task_id='streaming_data_from_api',
+        python_callable=streaming_data_from_api
+    )
